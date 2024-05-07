c545512de4470941fdd8eacc7a9e22f81d4914b5
akhzamov <abdulmalikakhzamov1@gmail.com>
2024-05-07 20:52:17 +0500

    edited

--- src/routes/application/dto/createApplication.dto.ts
+++ src/routes/application/dto/createApplication.dto.ts
@@ -1,12 +1,24 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class CreateApplicationDto {
   @IsNotEmpty()
   @ApiProperty()
   readonly name: string;
 
+  @ApiProperty()
+  readonly email?: string;
+
   @IsNotEmpty()
   @ApiProperty()
   readonly phone: string;
+
+  @ApiProperty()
+  readonly company: string;
+
+  @ApiProperty()
+  readonly description: string;
+
+  @ApiProperty()
+  readonly tagList?: string[];
 }
